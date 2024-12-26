877a5a8552f05bc7535037f660529ecd3104b29b
Gabriel <gabrielgatti27@gmail.com>
2024-12-26 16:24:21 -0300

    :memo: adding routes comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
+# A route in Rails refers to a line of code that pairs an HTTP Method and a URL path. 
+# The route also tells Rails which controller and action should respond to a request.
+
+# To create all of the CRUD routes for the application, we can use the resources method.
+
 Rails.application.routes.draw do
   resources :lists do
     resources :tasks, only: [:create, :update, :destroy]
   end
-  root "lists#index"
+  root "lists#index" # We can tell Rails the root route should render the Lists index action
 end
