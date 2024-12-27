c9b7d7e3b2fb0d939bd07157683f40351432adfb
Gabriel <gabrielgatti27@gmail.com>
2024-12-27 16:32:40 -0300

    :sparkles: add sidebar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  before_action :load_sidebar_lists
+
+  private
+
+  def load_sidebar_lists
+    @sidebar_lists = List.all   # Add a database query and assign it to an instance variable (starts with @) to make it available to the view
+  end
 end
