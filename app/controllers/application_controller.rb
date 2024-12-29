ea69274ea1ceda2b2a7b51e5412113543874c997
Gabriel <gabrielgatti27@gmail.com>
2024-12-29 16:05:50 -0300

    :warning: dealing with routing error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,19 @@ class ApplicationController < ActionController::Base
 
   before_action :load_sidebar_lists
 
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  def routing_error
+    redirect_to root_path, alert: "Página não encontrada."
+  end
+
   private
 
   def load_sidebar_lists
     @sidebar_lists = List.all   # Add a database query and assign it to an instance variable (starts with @) to make it available to the view
+  end
+
+  def record_not_found
+    redirect_to root_path, alert: "Registro não encontrado."
   end
 end
