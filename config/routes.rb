e52f9b0ace91dc282a28b22acb5453cef17c01c3
Gabriel Gatti <gabrielgatti27@gmail.com>
2026-04-24 20:57:00 -0300

    fix health check route order

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
-# A route in Rails refers to a line of code that pairs an HTTP Method and a URL path.
-# The route also tells Rails which controller and action should respond to a request.
-
-# To create all of the CRUD routes for the application, we can use the resources method.
-
 Rails.application.routes.draw do
   devise_for :users
   resources :lists do
@@ -12,8 +7,11 @@ Rails.application.routes.draw do
       end
     end
   end
-  root "lists#index" # We can tell Rails the root route should render the Lists index action
 
-  # Route to catch any other path
+  root "lists#index"
+
+  # Health check before catch-all
+  get "up" => "rails/health#show", as: :rails_health_check
+
   match "*path", to: "application#routing_error", via: :all
 end
